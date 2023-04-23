6f0bd0fedc112b2dbd72cf835cc22f92cafb1f2a
Nguyen-Van-Thanh-Cong <125261565+Nguyen-Van-Thanh-Cong@users.noreply.github.com>
2023-04-23 19:24:24 +0700

    Lab4-bai6

---
+++ Lab4B6/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+	CNKSP ksp=new CNKSP();
+	CNTCN tcn=new CNTCN();
+	
+	System.out.println("Luong CNKSP:");
+	ksp.nhap();
+	ksp.tinhluong();
+	ksp.in();
+	
+	System.out.println("Luong CNTCN:");
+	tcn.nhap();
+	tcn.tinhluong();
+	tcn.in();
+
+	}
+
+}
